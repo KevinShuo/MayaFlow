7a2b9312bb5ee3860ab1222c182482392f32df53
KevinShuo <wangshuoart@gmail.com>
2025-03-24 15:09:26 +0800

    aasxasx

---
+++ scripts/check_and_publish/src/view/task_choose.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from PySide2.QtWidgets import QWidget
+
+
+class TaskChooseUI(QWidget):
+    def __init__(self, parent=None):
+        super(TaskChooseUI, self).__init__(parent)
