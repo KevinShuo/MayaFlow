63e4f107a9cdcca8d3d9e4ac4b649dc3da22f574
KevinShuo <wangshuoart@gmail.com>
2025-04-22 17:31:34 +0800

    测试asset的startup 通过

--- bat/Maya2018.bat
+++ bat/Maya2018.bat
@@ -1,8 +1,10 @@
 @echo off
-
+set "XBMLANGPATH=%~dp0../resources"
+set "SCRIPTS_PATH=%~dp0../scripts"
 set "CGTM=C:\CgTeamWork_v7\bin\base"
-
-set "PYTHONPATH=C:\dev\maya\MayaFlow;C:\dev\maya\MayaFlow\scripts;C:\dev\maya\MayaFlow\libs\py2\site-packages;%CGTM%;%PYTHONPATH%"
+set "SRC_PATH=%~dp0../src/py2/abc"
 set "INFO_METHOD=file"
 set "PIPELINE_TYPE=abc"
+set "PYTHONPATH=C:\dev\maya\MayaFlow;C:\dev\maya\MayaFlow\scripts;C:\dev\maya\MayaFlow\libs\py2\site-packages;%CGTM%;%SRC_PATH%;%PYTHONPATH%"
+
 "C:\Program Files\Autodesk\Maya2018\bin\maya.exe"
